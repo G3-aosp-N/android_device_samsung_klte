b3771e43215d1d28d10ae7cc656fc245139f09fd
sooti <sooti85@gmail.com>
2017-07-02 12:18:51 +0000

    klte: use our vendor setup

--- aosp_klte.mk
+++ aosp_klte.mk
@@ -1,5 +1,7 @@
 $(call inherit-product, device/samsung/klte/full_klte.mk)
 
+# Use our common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 PRODUCT_NAME := aosp_klte
 PRODUCT_DEVICE := klte
